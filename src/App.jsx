5768d04c62ec7c4cead40a61d8e5f7271e6ad1b1
platisam <platisam@gmail.com>
2023-07-26 22:48:19 +0200

    importing the createBrowserRouter function and passing in an array of route objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+
+createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+]);
+
 const App = () => {
   return <div>Hello Vite!</div>;
 };
