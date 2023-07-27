dd7ad578655ab54afa2b80b3f72b4270824bf564
platisam <platisam@gmail.com>
2023-07-27 13:25:37 +0200

    adding useNavigation hook

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,8 +1,12 @@
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
 const AppLayout = () => {
+  const navigation = useNavigation();
+  //console.log(navigation);
+  const isLoading = navigation.state === "loading";
+
   return (
     <div>
       <Header />
