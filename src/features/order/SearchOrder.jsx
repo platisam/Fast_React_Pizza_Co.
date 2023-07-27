9ce4dde30a08f1c8680aa001198a5af368d3d66e
platisam <platisam@gmail.com>
2023-07-27 14:35:20 +0200

    added SearchOrder component

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const SearchOrder = () => {
+  const [query, setQuery] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Search order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
+};
+export default SearchOrder;
