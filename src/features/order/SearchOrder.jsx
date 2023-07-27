ba734f9114528c902288161ac40688ec460da69c
platisam <platisam@gmail.com>
2023-07-27 14:43:56 +0200

    fetching order from SearchOrder component

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SearchOrder = () => {
   const [query, setQuery] = useState("");
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!query) return;
+    navigate(`/order/${query}`);
+    setQuery("");
   }
 
   return (
