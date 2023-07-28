7377760e82d945ba8d8b6fcb18a4e984ebb9789d
platisam <platisam@gmail.com>
2023-07-28 13:42:39 +0200

    changed title

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
@@ -43,7 +42,7 @@ function CreateOrder() {
 
   return (
     <div>
-      <h2>Ready to order? Let's go!</h2>
+      <h2>Ready to order? Lets go!</h2>
 
       {/*<Form method="POST" action="/order/new">*/}
       <Form method="POST">
@@ -93,7 +92,7 @@ export async function action({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
 
-  console.log(data);
+  //console.log(data);
 
   const order = {
     ...data,
@@ -108,7 +107,7 @@ export async function action({ request }) {
 
   if (Object.keys(errors).length > 0) return errors;
 
-  //mif everything is ok, create new order and redirect
+  //if everything is ok, create new order and redirect
   const newOrder = await createOrder(order);
 
   return redirect(`/order/${newOrder.id}`);
