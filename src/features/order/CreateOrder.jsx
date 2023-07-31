3952b12b8c5945a4a9d9968f0d3deeba52a9e4f5
platisam <platisam@gmail.com>
2023-07-31 18:15:35 +0200

    adding focus and disabled state to button in tailwind

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -79,7 +79,7 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting} className="bg-yellow-400 uppercase font-semibold tracking-wide text-stone-800 py-3 px-4 inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300">
+          <button disabled={isSubmitting} className="bg-yellow-400 uppercase font-semibold tracking-wide text-stone-800 py-3 px-4 inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 active:bg-slate-400 disabled:cursor-not-allowed disabled:">
             {isSubmitting ? "Placing order..." : "Order now"}
           </button>
         </div>
