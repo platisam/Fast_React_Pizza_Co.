f98c7f03600cb776b20a0fc4da04f0791893a21b
platisam <platisam@gmail.com>
2023-07-31 22:24:10 +0200

    reusing styles with react components

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,13 +1,16 @@
-const Button = ({children, disabled}) => {
+import { Link } from "react-router-dom"
+
+const Button = ({children, disabled, to}) => {
+
+  const className = "bg-yellow-400 uppercase font-semibold tracking-wide text-stone-800 py-3 px-4 inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 active:bg-slate-400 disabled:cursor-not-allowed sm:px-6 sm:py-4";
+  
+
+  if(to) return <Link to={to} className={className}>{children}</Link>
+
   return (
-    <button disabled={disabled} className="bg-yellow-400 uppercase 
-    font-semibold tracking-wide text-stone-800 py-3 px-4 inline-block 
-    rounded-full hover:bg-yellow-300 transition-colors duration-300 
-    focus:outline-none focus:ring focus:ring-yellow-300 
-  focus:bg-yellow-300 focus:ring-offset-2 active:bg-slate-400 
-    disabled:cursor-not-allowed sm:px-6 sm:py-4">
-      {children}
-    </button>
+    <button disabled={disabled} className={className}>{children}</button>
+    
+  
   )
 }
 export default Button
--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -1,9 +1,13 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 const LinkButton = ({children, to}) => {
+  const navigate = useNavigate();
+  const className = 'text-sm text-blue-500 hover:text-blue-900 hover:underline';
+
+  if(to === '-1') return <button className={className} onClick={() => navigate(-1)}>{children}</button>
+
   return <Link 
-  to={to} className='text-sm text-blue-500 
-  hover:text-blue-900 hover:underline'>
+  to={to} className={className}>
   {children}
 </Link> 
 }
