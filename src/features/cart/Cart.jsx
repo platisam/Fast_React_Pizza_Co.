493e460c935196a11f048a437538b7f71b82ae25
platisam <platisam@gmail.com>
2023-08-02 23:21:26 +0200

    displaying EmptyCart component

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -3,6 +3,7 @@ import Button from "../../ui/Button";
 import CartItem from "./CartItem";
 import { useDispatch, useSelector } from "react-redux";
 import { clearCart, getCart } from "./cartSlice";
+import EmptyCart from "./EmptyCart";
 
 /*const fakeCart = [
   {
@@ -32,6 +33,8 @@ function Cart() {
   const username = useSelector((state) => state.user.username);
   const cart = useSelector(getCart);
   const dispatch = useDispatch();
+
+  if (!cart.length) return <EmptyCart />;
 
   return (
     <div className="py-3 px-4">
