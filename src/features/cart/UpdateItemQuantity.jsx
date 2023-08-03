9e46c9ef50340055cede868a5e0b5b8011351d38
platisam <platisam@gmail.com>
2023-08-03 18:29:57 +0200

    dispatching select function for decrease and increase quantity

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -1,10 +1,23 @@
+import { useDispatch } from "react-redux";
 import Button from "../../ui/Button";
+import { decreaseItemQuantity, increaseItemQuantity } from "./cartSlice";
 
 const UpdateItemQuantity = ({ pizzaId }) => {
+  const dispatch = useDispatch();
   return (
     <div className="flex gap-1 items-center md:gap-3">
-      <Button type="round">-</Button>
-      <Button type="round">+</Button>
+      <Button
+        type="round"
+        onClick={() => dispatch(decreaseItemQuantity(pizzaId))}
+      >
+        -
+      </Button>
+      <Button
+        type="round"
+        onClick={() => dispatch(increaseItemQuantity(pizzaId))}
+      >
+        +
+      </Button>
     </div>
   );
 };
