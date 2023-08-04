a6a69246a6b76fd549dce2179f59305d3608a78b
platisam <platisam@gmail.com>
2023-08-04 17:45:43 +0200

    adding extraReducers with pending, fulfilled and rejected state

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -20,12 +20,17 @@ export const fetchAddress = createAsyncThunk(
     const addressObj = await getAddress(position);
     const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
     // 3) Then we return an object with the data that we are interested in
+    // Payload of the FULFILLED state
     return { position, address };
   }
 );
 
 const initialState = {
   username: "",
+  status: "idle",
+  position: {},
+  address: "",
+  error: "",
 };
 
 const userSlice = createSlice({
@@ -36,6 +41,20 @@ const userSlice = createSlice({
       state.username = action.payload;
     },
   },
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchAddress.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchAddress.fulfilled, (state, action) => {
+        state.position = action.payload.position;
+        state.address = action.payload.address;
+        state.state = "idle";
+      })
+      .addCase(fetchAddress.rejected, (state, action) => {
+        state.status = "error";
+        state.error = action.error.message;
+      }),
 });
 
 export const { updateName } = userSlice.actions;
