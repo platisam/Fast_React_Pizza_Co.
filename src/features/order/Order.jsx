57979186562eefd55c986158e1893f1be4a8fcdc
platisam <platisam@gmail.com>
2023-08-04 22:18:51 +0200

    added UpdateOrder component

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,6 +1,7 @@
 // Test ID: IIDSAT
 import { useFetcher, useLoaderData } from "react-router-dom";
 import OrderItem from "./OrderItem";
+import UpdateOrder from "./UpdateOrder";
 import { getOrder } from "../../services/apiRestaurant";
 import {
   calcMinutesLeft,
@@ -123,6 +124,8 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
---
+++ src/features/order/UpdateOrder.jsx
@@ -0,0 +1,6 @@
+import Button from "../../ui/Button";
+
+const UpdateOrder = ({ order }) => {
+  return <Button type="primary">Make priority</Button>;
+};
+export default UpdateOrder;
