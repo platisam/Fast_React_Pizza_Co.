39c657ad8cd90845bd3db8981617ce826864aa7a
platisam <platisam@gmail.com>
2023-08-04 22:42:07 +0200

    adding fetcher.Form element

--- src/features/order/UpdateOrder.jsx
+++ src/features/order/UpdateOrder.jsx
@@ -1,6 +1,19 @@
+import { useFetcher } from "react-router-dom";
 import Button from "../../ui/Button";
 
 const UpdateOrder = ({ order }) => {
-  return <Button type="primary">Make priority</Button>;
+  const fetcher = useFetcher();
+
+  return (
+    <fetcher.Form method="PATCH" className="text-right">
+      <Button type="primary">Make priority</Button>
+    </fetcher.Form>
+  );
 };
+
 export default UpdateOrder;
+
+export async function action({ request, params }) {
+  console.log("update");
+  return null;
+}
